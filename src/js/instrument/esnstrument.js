98ace3487c74b9ef24d480d95e73442559a3c9ae
Christoffer Quist Adamsen <quist@cs.au.dk>
2017-03-27 13:39:30 +0200

    bugfix

--- src/js/instrument/esnstrument.js
+++ src/js/instrument/esnstrument.js
@@ -956,11 +956,22 @@ if (typeof J$ === 'undefined') {
             callee = node.id.name;
         }
 
-        var ret = replaceInStatement(
-            "var " + node.currentFunctionVar + " = " + logFunctionEnterFunName + "(" + RP + "1, " + callee + ", this, arguments)",
-            getIid()
-        );
-        transferLoc(ret[0].declarations[0].init, node);
+        var ret = null;
+        if (node.currentFunctionVar === callee) {
+            ret = replaceInStatement(
+                logFunctionEnterFunName + "(" + RP + "1, " + callee + ", this, arguments)",
+                getIid(node)
+            );
+
+            transferLoc(ret[0].expression, node);
+        } else {
+            ret = replaceInStatement(
+                "var " + node.currentFunctionVar + " = " + logFunctionEnterFunName + "(" + RP + "1, " + callee + ", this, arguments)",
+                getIid(node)
+            );
+
+            transferLoc(ret[0].declarations[0].init, node);
+        }
         return ret;
     }
 
